2a2a82e7a125639894fab7f18b8b89145ed4d5c6
unknown <vkung15@gmail.com>
2019-04-03 12:45:42 -0400

    Routing, Controllers, and the View Assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,12 @@ describe("routes : static", () => {
       });
     });
   });
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", () => {
+      request.get("http://localhost:3000/about", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+      });
+    });
+  });
 });
